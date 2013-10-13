4f671110412334ef960d9d2f79d3aafdbea045be
kathyz <kathy.m.zhou@gmail.com>
2013-10-12 20:09:24 -0400

    added rap genius stuffs

--- karaokeMachine/content_script.js
+++ karaokeMachine/content_script.js
@@ -1,21 +1,36 @@
 
 //console.log("youtube content", $("body"));
 
+//  THE YOUTUBE VID
 function loadVideo () {
 	
-	var first_video = $("#results > ol > li:first-child .yt-lockup-title a")[0];
+	//console.log("url", window.location.href);
+	// Checks if it is youtube, then parses the DOM
+	if ( (window.location.href).indexOf("youtube") !== -1 ){
+		var first_video = $("#results > ol > li:first-child .yt-lockup-title a")[0];
 
-	var vid_link = first_video.href;
+		var vid_link = first_video.href;
 
-	console.log("video", first_video);
-	console.log("vid link", vid_link);
+		console.log("video", first_video);
+		console.log("vid link", vid_link);
 
-	window.location = vid_link;
+		//  Loads in the video
+		window.location = vid_link;
+	}
 
+}
+
+//  THE LYRICS
+function loadRapGenius () {
+	
 }
 
 $(function () {
 	
+
+
 	loadVideo();
+
+	loadRapGenius();
 
 });
--- karaokeMachine/popup.js
+++ karaokeMachine/popup.js
@@ -16,13 +16,22 @@ $(function () {
 
     // Youtube song search
     // End query to make sure content script doesnt load on every Youtube page
-    var songQ = "http://www.youtube.com/results?search_query=" + songInput + "&brenda=ok";
-    bkg.console.log("song query", songQ);
+    var videoQ = "http://www.youtube.com/results?search_query=" + songInput + "&brenda=ok";
+    bkg.console.log("song query", videoQ);
 
-    chrome.tabs.create({'url': songQ}, function(tab) {
+    // Rap Genius song search
+    // End query for same purpose
+    var lyricsQ = "http://rapgenius.com/search?hide_unexplained_songs=false&q=" + songInput + "&brenda=ok";
+
+    //  Video tab to create
+    chrome.tabs.create({'url': videoQ}, function(tab) {
       bkg.console.log("tab", tab);
     });
 
+    //  Lyrics tab to create
+    chrome.tabs.create({'url': lyricsQ}, function(tab) {
+      bkg.console.log("tab", tab);
+    });
 
   });
 
